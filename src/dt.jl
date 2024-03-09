626b27945c95beb8c35fd22c93cd5ef242112225
Arcueid <zqy_edu@163.com>
2024-03-09 10:59:05 +0800

    2024.3.9 10:55

--- src/dt.jl
+++ src/dt.jl
@@ -1,10 +1,8 @@
-using DecisionTree, MLJ
+using DecisionTree, MLJ, JLD2
 using CSV, DataFrames
 using LinearAlgebra
 
 function Mainf()
-    # train_x = (0:0.02:1)
-    # train_y = randn(2, length(train_x))
     n_basis = 6
 
     train_set = CSV.read("./stokes_up.csv", DataFrame, header=false)
@@ -13,33 +11,42 @@ function Mainf()
     y = Array(train_set[2:end, :])
     u, s, v = svd(y) 
     Z = u[:, 1:n_basis]
-    paras = Z' * y
-    paras = Array(transpose(paras))
+
+    paras = Z' * y 
+    paras = Array(
+                  transpose(paras)
+                #   paras
+                  )
+
     _p = []
     for i in eachindex(paras[:, 1])
         push!(_p, paras[i, :])
     end
     paras = _p
 
+    # paras = Array(randn(length(x[: ,1])))
+
+
     model = build_forest(paras, x)
     apply_forest(model, [0.75])
-    # n, m,L = 10^3, 5,4
-    # features = randn(n, m)
-    # weights = rand( m,L)
-    # labels = [features[[i],:]*weights for i in 1:n]
-    # model = build_tree(labels, features)
-    # # apply learned model
-    # apply_tree(model, [-0.9,3.0,5.1,1.9,0.0])
-
-    # RandomForest = @load RandomForestRegressor pkg = DecisionTree
-    # myrandforest = RandomForest(max_depth = -1,
-    #                             n_trees = 10,
-    #                             # min_samples_split = 5,
-    #                             min_samples_leaf = 2
+    paras
+    @save "./2024-3-2/test.model" model
+    
+    # ! bugs with MLJ.jl
+    # RandomForest = MLJ.@load RandomForestRegressor pkg = DecisionTree
+    # myrandforest = RandomForest(#max_depth = -1,
+    # #                             n_trees = 10,
+    # #                             # min_samples_split = 5,
+    # #                             min_samples_leaf = 2
     #                             )
-    # # X, y = @load_iris
+    
+    # # # # b = fit_data_scitype(myrandforest)
     # mach = machine(myrandforest, x, paras) |> MLJ.fit!
-    # # println(X)
+
+    # scitype(paras)
 end
 
 Mainf()
+
+
+# TEST TEST
---
+++ src/mlj.jl
@@ -0,0 +1,11 @@
+# * 使用MLJ包进行机器学习
+# * ArcueidCroft@SCU.math
+# * zqy_edu@163.com
+# * Last edit: 2024.3.6 03:06
+
+using MLJ, CSV, DataFrames
+using LinearAlgebra
+
+function Mainf()
+    
+end
